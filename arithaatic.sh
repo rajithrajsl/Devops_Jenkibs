2d120a407a899128f27134c62257e66f7f92cb9a
RajithRaj <rajithraj3@gmail.com>
2020-07-29 13:02:20 +0530

    First Commit

---
+++ arithaatic.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+num1=10
+num2=5
+
+echo "sum is : $(( num1 +n um2 ))"
+echo "diff is : $(( num1 - num2 ))"
+echo "pdt is : $(( num1 * num2 ))"
+echo "div is : $(( num1 / num2 ))"
+echo "quotient is : $(( num1 % num2 ))"
+#or
+echo "$num1 + $num2" | bc
+echo "$num1 - $num2" | bc
+echo "$num1 / $num2" | bc
+echo "$num1 * $num2" | bc
+#using expr
+
+echo "sum is : $( expr $num1 + $num2 )"
+#in multiplication
+echo "pdt is : $( expr $num1 \* $num2 )"
+
+n1=20.5
+n2=5
+echo "$n1 + $n2" | bc
+#for division
+echo "scale=2;$n1 / $n2" | bc
+
+num=4
+echo "scale=2;sqrt($num)" | bc -l
+
+
